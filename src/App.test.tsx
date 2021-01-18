19f211797507ace140ed61a784d787399b2fa79f
Nicolas Corbalan <outreef@hotmail.com>
2021-01-18 01:17:23 +0100

    testing with getByLabelText and getByPlaceholderText

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,13 +3,27 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('When everyhting is ok', () => {
+
+  beforeEach(() => {
+    render (<App />)
+  })
   test('should render the App component without crashing', () => {
-    render(<App />)
     screen.debug();
   });
 
   test('should select the children that is being passed to the CustomInput component', () => {
-    render(<App />)
     screen.getByText(/Input/);
+  });
+
+  test('should select the input element by its role', () => {
+    screen.getByRole('textbox');
+  });
+
+  test('should select a label by its text', () => {
+    screen.getByLabelText('Input:')
+  });
+
+  test('should select input element by placeholder text', () => {
+    screen.getByPlaceholderText('Example');
   })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,10 @@ function CustomInput({children, value, onChange}: CustomInputProps) {
     <label htmlFor="search">
       {children}
     </label>
-    <input id="search" type="text" value={value} onChange={onChange}/>
+      <input placeholder="Example" id="search" 
+      type="text" 
+      value={value} 
+      onChange={onChange}/>
   </div>
   )
 }
