f0b245cf9fd5760d40139310ef8c774e8ef4fd45
Nicolas Corbalan <outreef@hotmail.com>
2021-01-24 01:33:12 +0100

    mock getUser to fetch API once

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
+import {getUser} from './get-user';
+import {mocked} from 'ts-jest/utils';
+
+jest.mock('./get-user');
+const mockGetUser = mocked(getUser, true)
 
 describe('When everyhting is ok', () => {
 
-  beforeEach(() => {
-    render (<App />)
+  beforeEach( async () => {
+    render (<App />);
+    await waitFor(() => {expect(mockGetUser).toHaveBeenCalled()})
   })
   test('should render the App component without crashing', () => {
     screen.debug();
@@ -30,4 +36,14 @@ describe('When everyhting is ok', () => {
   test('should select the input element by its role with queryByRole', () => {
     expect(screen.queryByRole('textbox'));
   });
+})
+
+describe('When the component fetches the user successfully', () => {
+  beforeEach(() => {
+    mockGetUser.mockClear()
+  });
+  test('Should call getUser once', async () => {
+    render(<App />);
+    await waitFor(() => expect(mockGetUser).toHaveBeenCalledTimes(1));
+  })
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {getUser, User} from './get-user';
 
 
 
 function App() {
   const [text, setText] = useState('');
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await getUser();
+      setUser(user);
+    }; 
+    fetchUser();
+  }, [])
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setText(event.target.value);
   }
   return (
   <div>
+    {user ? <p>Username: {user.name}</p> : null}
     <CustomInput value={text} onChange={handleChange}>
       Input:
     </CustomInput>
